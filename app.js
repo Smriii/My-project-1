7e44c0efe235dfcce82a5bebe468568898d1cecc
Smriii <117383275+Smriii@users.noreply.github.com>
2024-12-31 00:07:41 +0530

    Updated app.js to use PORT environment variable

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const express=require("express")
+const app=express()
 
+app.get("/login",(req,res)=>{
+    res.send("login page");
+})
 
+app.get("/about",(req,res)=>{
+    res.send("about page");
+})
 
-
-app.listen(3000)
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
